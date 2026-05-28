e14f6ec624101fa86fdcddd48bc7219397f01d2e
Chubi <okafor.chinwuba@gmail.com>
2026-05-28 12:50:34 +0100

    Add iframe save flow objects to workOrder_view

---
+++ Object Repository/Pages_NEW/WorkOrder_View/btn_savePath.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_savePath</name>
+   <tag></tag>
+   <elementGuidId>81edcf6a-83d5-4d7e-8a2d-80788b25fb03</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>.flex-row [type=&quot;button&quot;]:nth-child(2)</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' flex-row ')]//*[(@type = 'button') and (position() = 2)]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot;Save Path&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>52d6dd66-cc4b-40f9-8e09-547c321ac7b9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>206bfed1-2c43-47fa-9069-defb0816d654</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>styles_root__SH6LT styles_primary__3Ut_x styles_medium__vX6fc styles_outlined__55Re2</value>
+      <webElementGuid>43f01863-7ea9-4794-b984-d5241a6803d8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Save Path</value>
+      <webElementGuid>d50455db-5398-48be-a8da-13783badbc1c</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-4c47e5d1802549426a434103940fbb14</value>
+      <webElementGuid>c86b8b41-4d4b-44e0-a376-6635dd7a409b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' flex-row ')]//*[(@type = 'button') and (position() = 2)]</value>
+      <webElementGuid>dacb5c52-87a1-4833-88bf-a28bab1b67da</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/Pages_NEW/WorkOrder_View/iframe_cdeWidget</value>
+      <webElementGuid>46b073e0-57c9-4a31-b794-83aca96776e5</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[@class and contains(concat(' ', normalize-space(@class), ' '), ' flex-row ')]//*[(@type = 'button') and (position() = 2)]</value>
+      <webElementGuid>0a56f5c3-8889-4456-b201-6baa9ea618ea</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[@type = 'button' and (text() = 'Save Path' or . = 'Save Path')]</value>
+      <webElementGuid>df334c9a-a3a6-4a8e-bdae-fb9f9bc85528</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Pages_NEW/WorkOrder_View/img_fileUploadIcon.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>img_fileUploadIcon</name>
+   <tag></tag>
+   <elementGuidId>98a52751-96cc-44fd-bdca-ea369d619eb4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>tr:nth-child(1) [alt=&quot;file\ upload\ icon&quot;]</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[(name() = 'tr') and (position() = 1)]//*[@alt = 'file upload icon']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=row[name=&quot;PAST.pdf 2026-05-26 16:55:03 2026-05-28 12:38:37 api support View | Download | Sign | file upload icon | Delete |&quot;i] >> internal:attr=[title=&quot;Upload To Policy Folder&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>img</value>
+      <webElementGuid>05607931-16ca-4303-96a9-7a5eab2be45e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>src</name>
+      <type>Main</type>
+      <value>img/policy-folder.png</value>
+      <webElementGuid>66b56c13-5c8d-4cdf-8652-51aeba5903ee</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>alt</name>
+      <type>Main</type>
+      <value>file upload icon</value>
+      <webElementGuid>ba31fd33-d9c5-416f-83fa-3d1bc3a649d6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>parent</name>
+      <type>Main</type>
+      <value>md5.v1-31f47feecd47b61017daf554df51b38d</value>
+      <webElementGuid>3b52b3f9-4bf6-48c3-b606-14260c25828d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[(name() = 'tr') and (position() = 1)]//*[@alt = 'file upload icon']</value>
+      <webElementGuid>19d87199-643d-4a3c-9261-2e6b627041a9</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//*[(name() = 'tr') and (position() = 1)]//*[@alt = 'file upload icon']</value>
+      <webElementGuid>e3709090-10f8-4630-b413-6db545935c1f</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//img[@src = 'img/policy-folder.png' and @alt = 'file upload icon']</value>
+      <webElementGuid>f9763643-2351-4132-b595-47e99c21aa69</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
